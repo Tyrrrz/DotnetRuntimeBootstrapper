9c1855716932a6ec7797b55f11ad8c9f7c05c681
Tyrrrz <tyrrrrrr@gmail.com>
2021-06-08 22:11:04 +0300

    Improve code reuse

--- DotnetRuntimeBootstrapper.Executable/Utils/Http.cs
+++ DotnetRuntimeBootstrapper.Executable/Utils/Http.cs
@@ -14,27 +14,30 @@ namespace DotnetRuntimeBootstrapper.Executable.Utils
             return request;
         }
 
-        public static string GetContentString(string url)
+        private static Stream GetContentStream(string url, out long length)
         {
             var request = CreateRequest(url);
             var response = request.GetResponse();
 
-            using var stream = response.GetResponseStream();
-            using var reader = new StreamReader(stream ?? Stream.Null);
+            length = response.ContentLength;
+            return response.GetResponseStream() ?? Stream.Null;
+        }
+
+        public static Stream GetContentStream(string url) =>
+            GetContentStream(url, out _);
+
+        public static string GetContentString(string url)
+        {
+            using var stream = GetContentStream(url);
+            using var reader = new StreamReader(stream);
 
             return reader.ReadToEnd();
         }
 
         public static void DownloadFile(string url, string outputFilePath, Action<double>? handleProgress)
         {
+            using var source = GetContentStream(url, out var contentLength);
             using var destination = File.Create(outputFilePath);
-
-            var request = CreateRequest(url);
-            var response = request.GetResponse();
-
-            using var source = response.GetResponseStream();
-            if (source is null)
-                return;
 
             var buffer = new byte[81920];
 
@@ -42,11 +45,13 @@ namespace DotnetRuntimeBootstrapper.Executable.Utils
             int bytesCopied;
             do
             {
+                // Copy data
                 bytesCopied = source.Read(buffer, 0, buffer.Length);
                 destination.Write(buffer, 0, bytesCopied);
 
+                // Report progress
                 totalBytesCopied += bytesCopied;
-                handleProgress?.Invoke(1.0 * totalBytesCopied / response.ContentLength);
+                handleProgress?.Invoke(1.0 * totalBytesCopied / contentLength);
             } while (bytesCopied > 0);
         }
     }
