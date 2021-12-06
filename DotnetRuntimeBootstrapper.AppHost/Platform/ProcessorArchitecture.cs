076f09f2d0829774deb40066220b25c5365a83de
Tyrrrz <1935960+Tyrrrz@users.noreply.github.com>
2021-12-06 22:38:39 +0200

    Simplify GetMoniker() for arch

--- DotnetRuntimeBootstrapper.AppHost/Platform/ProcessorArchitecture.cs
+++ DotnetRuntimeBootstrapper.AppHost/Platform/ProcessorArchitecture.cs
@@ -1,6 +1,4 @@
-﻿using System;
-
-namespace DotnetRuntimeBootstrapper.AppHost.Platform
+﻿namespace DotnetRuntimeBootstrapper.AppHost.Platform
 {
     internal enum ProcessorArchitecture
     {
@@ -15,13 +13,7 @@ namespace DotnetRuntimeBootstrapper.AppHost.Platform
         public static bool Is64Bit(this ProcessorArchitecture arch) =>
             arch is ProcessorArchitecture.X64 or ProcessorArchitecture.Arm64;
 
-        public static string GetMoniker(this ProcessorArchitecture arch) => arch switch
-        {
-            ProcessorArchitecture.X86 => "x86",
-            ProcessorArchitecture.X64 => "x64",
-            ProcessorArchitecture.Arm => "arm",
-            ProcessorArchitecture.Arm64 => "arm64",
-            _ => throw new ArgumentOutOfRangeException(nameof(arch), "Unknown processor architecture.")
-        };
+        public static string GetMoniker(this ProcessorArchitecture arch) =>
+            arch.ToString().ToLowerInvariant();
     }
 }
