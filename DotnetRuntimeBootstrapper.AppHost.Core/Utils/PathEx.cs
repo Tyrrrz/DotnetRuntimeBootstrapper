09c52cfdb2e6108e014022bef40e81919eadc621
Tyrrrz <1935960+Tyrrrz@users.noreply.github.com>
2024-11-12 22:24:59 +0200

    Use .NET 9

--- DotnetRuntimeBootstrapper.AppHost.Core/Utils/PathEx.cs
+++ DotnetRuntimeBootstrapper.AppHost.Core/Utils/PathEx.cs
@@ -1,9 +1,10 @@
-﻿using System.IO;
+﻿using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 
 namespace DotnetRuntimeBootstrapper.AppHost.Core.Utils;
 
 internal static class PathEx
 {
-    public static string Combine(params string[] paths) => paths.Aggregate(Path.Combine);
+    public static string Combine(params IEnumerable<string> paths) => paths.Aggregate(Path.Combine);
 }
